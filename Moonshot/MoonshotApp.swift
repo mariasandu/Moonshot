729e86f71ae75a24f6dff28ef1fa0f73bc2aff49
Maria Sandu <Maria.Sandu@emids.com>
2023-09-09 10:18:06 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Maria Sandu on 09.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
